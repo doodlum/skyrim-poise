2e3d31435ca69a226ce54ed9e6c2ad66e5669622
Tim <15017472+doodlum@users.noreply.github.com>
2022-06-10 13:57:05 +0100

    Remove unneeded addition

--- src/PoiseAVHUD.cpp
+++ src/PoiseAVHUD.cpp
@@ -4,7 +4,7 @@ float PoiseAVHUD::GetMaxSpecial([[maybe_unused]] RE::Actor* a_actor)
 {
 	if (a_actor) {
 		auto manager = AVManager::GetSingleton();
-		return manager->GetActorValueMax("Poise", a_actor) + FLT_MIN;
+		return manager->GetActorValueMax("Poise", a_actor);
 	}
 	return 1.0f;
 }
