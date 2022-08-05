70681e43302be982b4cb64502eea79212186b817
Tim <15017472+doodlum@users.noreply.github.com>
2022-08-05 14:39:26 +0100

    Fixes

--- src/Hooks/PoiseAV.h
+++ src/Hooks/PoiseAV.h
@@ -2,9 +2,10 @@
 
 #include "ActorValues/AVInterface.h"
 
-// variables
 //static float* g_deltaTime = (float*)RELOCATION_ID(523660, 410199).address();          // 2F6B948, 30064C8
 //static float* g_deltaTimeRealTime = (float*)RELOCATION_ID(523661, 410200).address();  // 2F6B94C, 30064CC
+
+static float& g_deltaTime = (*(float*)RELOCATION_ID(523660, 410199).address());
 
 class PoiseAV : public AVInterface
 {
@@ -14,25 +15,20 @@ public:
 		Hooks::Install();
 	}
 
-	inline static const char* g_avName = "Poise";
-
-	bool CanDamageActor(RE::Actor* a_actor);
-
-	float GetBaseActorValue(RE::Actor* a_actor);
-
-	float GetActorValueMax(RE::Actor* a_actor);
-
-	void DamageAndCheckPoise(RE::Actor* a_target, RE::Actor* a_aggressor, float a_poiseDamage);
-
-	void Update(RE::Actor* a_actor, float a_delta);
-
-	void GarbageCollection();
-
 	static PoiseAV* GetSingleton()
 	{
 		static PoiseAV avInterface;
 		return &avInterface;
 	}
+
+	inline static const char* g_avName = "Poise";
+
+	bool  CanDamageActor(RE::Actor* a_actor);
+	float GetBaseActorValue(RE::Actor* a_actor);
+	float GetActorValueMax(RE::Actor* a_actor);
+	void  DamageAndCheckPoise(RE::Actor* a_target, RE::Actor* a_aggressor, float a_poiseDamage);
+	void  Update(RE::Actor* a_actor, float a_delta);
+	void  GarbageCollection();
 
 	//static void TryPushActorAway(RE::Actor* target, [[maybe_unused]] float staggerMult, RE::Actor* aggressor)
 	//{
@@ -61,12 +57,62 @@ public:
 		ForceFullBodyStagger = RE::TESForm::LookupByID(0x10CED7)->As<RE::TESFaction>();
 	}
 
+	bool            appliedStagger = false;
+	std::thread::id staggerThread;
+
+	// POISE animations. Didn't like it
+	//static void TryStaggerCustom(RE::Actor* a_target, float a_staggerMult, RE::Actor* a_aggressor)
+	//{
+	//	float stagDir;
+	//	if (a_target == a_aggressor || a_aggressor == nullptr) {
+	//		stagDir = 0.0f;
+	//	} else {
+	//		auto hitPos = a_target->GetPosition();
+	//		auto heading = a_aggressor->GetHeadingAngle(hitPos, false);
+	//		stagDir = (heading >= 0.0f) ? heading / 360.0f : (360.0f + heading) / 360.0f;
+	//	}
+	//	a_target->SetGraphVariableFloat("staggerDirection", stagDir);  // set direction
+	//	a_target->SetGraphVariableFloat("staggerMagnitude", a_staggerMult);
+	//	static RE::BSFixedString str = NULL;
+	//	if (a_staggerMult <= 0.25) {
+	//		if (stagDir > 0.25f && stagDir < 0.75f) {
+	//			str = "poise_small_start_fwd";
+	//		} else {
+	//			str = "poise_small_start";
+	//		}
+	//	} else if (a_staggerMult <= 0.75) {
+	//		if (stagDir > 0.25f && stagDir < 0.75f) {
+	//			str = "poise_med_start";
+	//		} else {
+	//			str = "poise_med_start_fwd";
+	//		}
+	//	} else if (a_staggerMult <= 1) {
+	//		if (stagDir > 0.25f && stagDir < 0.75f) {
+	//			str = "poise_large_start";
+	//		} else {
+	//			str = "poise_large_start_fwd";
+	//		}
+	//	} else if (a_staggerMult > 1.0) {
+	//		if (stagDir > 0.25f && stagDir < 0.75f) {
+	//			str = "poise_largest_start";
+	//		} else {
+	//			str = "poise_large_start_fwd";
+	//		}
+	//	}
+	//	a_target->NotifyAnimationGraph(str);  // play animation
+	//	a_target->actorState2.staggered = true;
+	//}
+
 	static void TryStagger(RE::Actor* a_target, float a_staggerMult, RE::Actor* a_aggressor)
 	{
+		GetSingleton()->appliedStagger = true;
+		GetSingleton()->staggerThread = std::this_thread::get_id();
 		using func_t = decltype(&TryStagger);
 		REL::Relocation<func_t> func{ REL::RelocationID(36700, 37710) };
-		func(a_target, -a_staggerMult, a_aggressor);
+		func(a_target, a_staggerMult, a_aggressor);
 	}
+
+
 
 	static void RemoveFromFaction(RE::Actor* a_actor, RE::TESFaction* a_faction)
 	{
@@ -97,7 +143,7 @@ protected:
 			static void thunk(RE::PlayerCharacter* a_player, float a_delta)
 			{
 				func(a_player, a_delta);
-				GetSingleton()->Update(a_player, a_delta);
+				GetSingleton()->Update(a_player, g_deltaTime);
 			}
 			static inline REL::Relocation<decltype(thunk)> func;
 		};
@@ -107,7 +153,7 @@ protected:
 			static void thunk(RE::Actor* a_actor, float a_delta)
 			{
 				func(a_actor, a_delta);
-				GetSingleton()->Update(a_actor, a_delta);
+				GetSingleton()->Update(a_actor, g_deltaTime);
 			}
 			static inline REL::Relocation<decltype(thunk)> func;
 		};
@@ -116,9 +162,13 @@ protected:
 		{
 			static void thunk(INT32 entry, RE::Actor* target, RE::Actor* aggressor, float& staggerMult)
 			{
-				if (staggerMult > 0) {
-					func(entry, target, aggressor, staggerMult);
-				}
+				auto poiseAV = GetSingleton();
+				auto currentThread = std::this_thread::get_id();
+				if (poiseAV->appliedStagger && poiseAV->staggerThread == currentThread)
+					return;
+				if (poiseAV->staggerThread != currentThread)
+					logger::debug("Stagger attempted on another thread");
+				func(entry, target, aggressor, staggerMult);
 			}
 			static inline REL::Relocation<decltype(thunk)> func;
 		};
@@ -127,11 +177,12 @@ protected:
 		{
 			static void thunk(INT32 entry, RE::Actor* target, RE::Actor* aggressor, float& staggerMult)
 			{
-				if (staggerMult > 0.0f) {
+				auto poiseAV = GetSingleton();
+				auto currentThread = std::this_thread::get_id();
+				if (poiseAV->appliedStagger && poiseAV->staggerThread == currentThread)
+					poiseAV->appliedStagger = false;
+				else
 					func(entry, target, aggressor, staggerMult);
-				} else {
-					staggerMult = -staggerMult;
-				}
 			}
 			static inline REL::Relocation<decltype(thunk)> func;
 		};
